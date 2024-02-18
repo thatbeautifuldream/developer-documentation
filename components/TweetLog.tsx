3a53a67aae70aab95c3091086dbb737bda54ed1d
Milind Mishra <milind.mishra4@gmail.com>
2024-02-18 07:13:20 +0530

    [NEW] add tweet log and pending fix css module import error from react-tweet

---
+++ components/TweetLog.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Tweet } from "react-tweet";
+
+function TweetLog() {
+  return (
+    <div>
+      <Tweet id="1441040000000000000" />
+    </div>
+  );
+}
+
+export default TweetLog;
